62361f18aacb76189ff964570f9af5d2ee29c1f9
shourya <pihushourya100@gmail.com>
2026-03-03 18:39:44 +0530

    integrated yolo.onnx models into ros2 and integrated camera capture to only captrue when sent true.... modified 1.jpg for yolo testing

--- src/opencv_test/opencv_test/image_publisher.py
+++ src/opencv_test/opencv_test/image_publisher.py
@@ -1,38 +1,69 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
+from std_msgs.msg import Bool
 import cv2
 from cv_bridge import CvBridge
 
 class ImageCommander(Node):
     def __init__(self):
-        super().__init__('image_commander')
+        super().__init__('image_publisher')
 
-        self.declare_parameter("image_path", "/home/shourya/robot_ws/src/opencv_comparison/picture_to_send/1.jpg")
+        self.declare_parameter("image_path", "/home/shourya/robot_ws/src/opencv_test/picture_to_send/1.jpg")
+        self.declare_parameter("live_stream", True)
+        self.declare_parameter("camera_index", 1)
+
+        self.live_stream = self.get_parameter("live_stream").get_parameter_value().bool_value
+        self.camera_index = self.get_parameter("camera_index").get_parameter_value().integer_value
         self.image_path = self.get_parameter("image_path").get_parameter_value().string_value
+        self.img = None
 
-        self.publisher_ = self.create_publisher(Image, '/camera_image', 10)
-        self.timer = self.create_timer(2.0, self.timer_callback)
+        if self.live_stream == True:
+            self.source = "camera" #camera
+            self.cam = cv2.VideoCapture(self.camera_index, cv2.CAP_V4L2)
+        else:
+            self.source = self.image_path
+            self.img = cv2.imread(self.image_path)
+            if self.img is None:
+                self.get_logger().error(f"Could not find image at {self.image_path}")
+                return
+
+        self.publisher_ = self.create_publisher(Image, '/camera/colour', 10)
+        self.cmp_pub = self.create_publisher(CompressedImage, '/camera/colour/compressed', 10)
+        self.cap_img_sub = self.create_subscription(Bool, "/capture_image", self.timer_callback, 10)
         
         self.bridge = CvBridge()
             
-    def timer_callback(self):
-        img = cv2.imread(self.image_path)
-        if img is None:
-            self.get_logger().error(f"Could not find image at {self.image_path}")
-            return
+    def timer_callback(self, msg):
+        if msg.data:
+            if self.live_stream == True:
+                ret, self.img = self.cam.read()
+                if not ret:
+                    self.get_logger().info("No image can be captured from camera")
+                    return
 
-        msg = self.bridge.cv2_to_imgmsg(img, encoding="bgr8")
-        
-        self.get_logger().info(f"Sending {self.image_path}...")
-        self.publisher_.publish(msg)
-        self.get_logger().info("Done!")
+            msg = self.bridge.cv2_to_imgmsg(self.img, encoding="bgr8")
+            cmp_msg = self.bridge.cv2_to_compressed_imgmsg(self.img)
+
+            self.get_logger().info(f"Sending {self.source}...")
+            self.publisher_.publish(msg)
+            self.cmp_pub.publish(cmp_msg)
+            self.get_logger().info("Done!")
+
+    def destroy_node(self):
+        if self.live_stream and hasattr(self, 'cam'):
+            self.cam.release()
+            self.get_logger().info("Camera hardware released.")
+        super().destroy_node()
+
+
 
 def main():
     rclpy.init()
     node = ImageCommander()
     rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
--- src/yolo/yolo/main_inference.py
+++ src/yolo/yolo/main_inference.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from rcl_interfaces.msg import ParameterDescriptor
 
 from sensor_msgs.msg import CompressedImage
 from std_msgs.msg import Bool
@@ -8,76 +10,92 @@ import cv2
 
 from yolo_detector import YOLODetector
 import os
-from ament_index_python.packages import get_package_share_directory
 
-
-
+#importing all the pkgs required
 class YOLO(Node):
     def __init__(self):
         super().__init__("yolo_inference")
 
-        self.declare_parameter("model_path", "models/my_model_2.onnx")
-        self.declare_parameter("save_images", True)
-        self.declare_parameter("camera_index", 2)
-        self.declare_parameter("conf_thres", 0.4)
+        #defining the args's descriptions
+        conf_thresh_arg_desc = ParameterDescriptor(description="Confidence threshold (0.0 to 1.0)")
+        model_path_arg_desc = ParameterDescriptor(description="Path to YOLO model in .ONNX format... also it should be relative to the yolo pkg folder")
+        save_images_arg_desc = ParameterDescriptor(description="true or false... whether to save images in yolo_pkg_folder/received_images")
 
-        self.CAMERA_INDEX = self.get_parameter("camera_index").get_parameter_value().integer_value
+        #declaring args
+        self.declare_parameter("model_path", "models/my_model_2.onnx", descriptor=model_path_arg_desc)
+        self.declare_parameter("save_images", True, descriptor=save_images_arg_desc)
+        self.declare_parameter("conf_thres", 0.5, descriptor=conf_thresh_arg_desc)
+
+        #passing arg's values to self variables
         self.MODEL_PATH = self.get_parameter("model_path").get_parameter_value().string_value
-        self.CONF_THRESH = self.get_parameter("conf_thres").get_parameter_value().float_value
-        self.save_images = self.get_parameter("save_images").get_parameter_value().string_value
+        self.CONF_THRESH = self.get_parameter("conf_thres").get_parameter_value().double_value
+        self.save_images = self.get_parameter("save_images").get_parameter_value().bool_value
 
+        #checking if the user has requested to live stream as well
         if self.save_images:
-            self.base_input_path = os.path.join(get_package_share_directory("yolo"), "received_images", "input") #the folder where the images from topic: /camera/colour are stored and also acts as the input for inference
-            self.results_path = os.path.join(get_package_share_directory("yolo"), "received_images", "results")#the folder where the resultant images are stored
-
+            #defining base paths for saving images
+            self.base_input_path = os.path.join("/home/shourya/robot_ws/src/yolo", "received_images", "input") #the folder where the images from topic: /camera/colour are stored and also acts as the input for inference
+            self.results_path = os.path.join("/home/shourya/robot_ws/src/yolo", "received_images", "results")#the folder where the resultant images are stored
+            #defining the final path
             self.final_input_path = os.path.join(self.base_input_path, "input.jpg")
             self.final_results_path = os.path.join(self.results_path, "results.jpg")
 
+        #loading the model in RAM so that we don't have to initialize it again in image callback
         self.get_logger().info("Loading YOLO model into RAM...")
         self.detector = YOLODetector(self.MODEL_PATH, conf_thres=self.CONF_THRESH)
 
-        self.colour_sub = self.create_subscription(CompressedImage, "/camera/colour", self.image_callback, 10)
+        #creating subs and pubs and bridges
+        self.colour_sub = self.create_subscription(CompressedImage, "/camera/colour/compressed", self.image_callback, 10)
         self.cracks_found_pub = self.create_publisher(Bool, "/cracks_found", 10)
         self.bridge = CvBridge()
 
+        #defining variables
         self.CLASSES = ["Fist bump", "Hello", "Thumbs down", "Thumbs up"]
         self.input_cv = None
 
-    def image_callback(self, msg):
-        try:
-            input_cv = self.bridge.compressed_imgmsg_to_cv2(msg, desired_encoding='bgr8')
-            cv2.imwrite(self.final_input_path, input_cv)
+        self.get_logger().info("waiting for image....")
 
-            self.run_inference(input_cv)
+    def image_callback(self, msg):
+        #this func will be called when an image is received
+        self.get_logger().info("received image")
+
+        try:#writing in try block to catch exceptions
+            input_cv = self.bridge.compressed_imgmsg_to_cv2(msg, desired_encoding='bgr8')#converting the ros2 msg to cv2 img
+            cv2.imwrite(self.final_input_path, input_cv)#writing it for debugging
+
+            self.run_inference(input_cv)#calling helper function while passing the cv2 img
+
         except Exception as e:
             self.get_logger().error(f"Image processing failed: {e}")
 
-
     def run_inference(self, frame):
-        detections = self.detector.detect(frame)
+        detections = self.detector.detect(frame)#running inference
 
-        self.get_logger().info(f"detections = {detections}")
-        cracks = Bool()
+        detected_names = []#creating list for appending the detections
+        for d in detections:#looping through each detections
+            name = self.CLASSES[d['class_id']]
+            detected_names.append(name)#appending them
 
-        if len(detections) >= 1:
-            self.get_logger().info(f"Detected {len(detections)} cracks")
+        cracks = Bool() #initializing the msg to be sent in /cracks_found
+        if len(detections) >= 1: #if there are more than or equal to 1 objects then print it in  console
+            self.get_logger().info(f"-----------objects detected: {', '.join(detected_names)}------")
             cracks.data = True
         else:
-            self.get_logger().info(f"Detected no cracks")
+            self.get_logger().info(f"Detected no objects")
             cracks.data = False
 
-        self.cracks_found_pub.publish(cracks)
-        self.get_logger().info(f"drawing boxes")
+        self.cracks_found_pub.publish(cracks)#publishing the data
 
-        for det in detections:
+        for det in detections: #actual drawing boxes part
             x1, y1, x2, y2 = det["box"]
-            label = f"{self.CLASSES[det['class_id']]}: {det['conf']:.2f}"
+            class_name = self.CLASSES[det['class_id']]
+            label = f"{class_name}: {det['conf']:.2f}"
 
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(frame, label, (x1, y1 - 10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        cv2.imwrite(self.final_results_path, frame)
+        cv2.imwrite(self.final_results_path, frame)#writing the results image to path
 
 
 def main():
@@ -86,7 +104,6 @@ def main():
     rclpy.spin(inference)
     inference.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == "__main__":
     main()
