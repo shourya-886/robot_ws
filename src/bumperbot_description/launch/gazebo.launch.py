3234de6c6e5057f4eba705aa88b7267ed8029074
shourya <pihushourya100@gmail.com>
2026-05-03 11:16:29 +0530

    added models and worlds from amazon, modified launch file to accomadate new worlds, found error in _ros2_control.xacro, fixed bug, everyhting is working

--- src/bumperbot_description/launch/gazebo.launch.py
+++ src/bumperbot_description/launch/gazebo.launch.py
@@ -1,10 +1,11 @@
 import os
+from os import pathsep
 from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -20,12 +21,19 @@ def generate_launch_description():
                                       description="Absolute path to robot urdf file"
     )
 
-    gazebo_resource_path = SetEnvironmentVariable(
-        name="GZ_SIM_RESOURCE_PATH",
-        value=[
-            str(Path(robot_description).parent.resolve())
-            ]
-        )
+    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="empty")
+
+    world_path = PathJoinSubstitution([
+        robot_description,
+        "worlds",
+        PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"]),
+        
+    ])
+
+    model_path = str(Path(robot_description).parent.resolve())
+    model_path += pathsep + os.path.join(robot_description, "models")
+
+    gazebo_resource_path = SetEnvironmentVariable("GZ_SIM_RESOURCE_PATH", model_path)
     
     ros_distro = os.environ["ROS_DISTRO"]
     is_ignition = "True" if ros_distro == "humble" else "False"
@@ -49,10 +57,9 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
-                launch_arguments=[
-                    ("gz_args", [" -v 4", " -r", " empty.sdf"]
-                    )
-                ]
+                launch_arguments={
+                    "gz_args": PythonExpression(["'", world_path, " -v 4 -r '"])
+                }.items()
              )
 
     gz_spawn_entity = Node(
@@ -76,6 +83,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         model_arg,
+        world_name_arg,
         gazebo_resource_path,
         robot_state_publisher_node,
         gazebo,
