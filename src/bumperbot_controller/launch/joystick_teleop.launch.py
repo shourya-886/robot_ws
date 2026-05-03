14873f8b949600a515a1cbc080bff55a5bb8c33b
shourya <pihushourya100@gmail.com>
2026-05-03 18:24:15 +0530

    added twist_mux for support with keyboard, no issues found, working in gazebo

--- src/bumperbot_controller/launch/joystick_teleop.launch.py
+++ src/bumperbot_controller/launch/joystick_teleop.launch.py
@@ -3,12 +3,15 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
+
+    bumperbot_controller_pkg = get_package_share_directory("bumperbot_controller")
 
     use_sim_time_arg = DeclareLaunchArgument(name="use_sim_time", default_value="True",
                                       description="Use simulated time"
@@ -29,10 +32,36 @@ def generate_launch_description():
                     {"use_sim_time": LaunchConfiguration("use_sim_time")}]
     )
 
+    twist_mux_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("twist_mux"),
+                "launch",
+                "twist_mux_launch.py",
+            )
+        ),
+        launch_arguments={
+            "cmd_vel_out": "bumperbot_controller/cmd_vel_unstamped",
+            "config_topic": os.path.join(bumperbot_controller_pkg, "config", "twist_mux_topics.yaml"),
+            "config_lock": os.path.join(bumperbot_controller_pkg, "config", "twist_mux_locks.yaml"),
+            "config_joy": os.path.join(bumperbot_controller_pkg, "config", "twist_mux_joys.yaml"),
+            "use_sim_time": LaunchConfiguration("use_sim_time")
+        }.items()
+    )  
+
+    twist_relay_node = Node(
+        package="bumperbot_controller",
+        executable="twist_relay.py",
+        name="twist_relay",
+        parameters=[{"use_sim_time": LaunchConfiguration("use_sim_time")}]
+    )
+
     return LaunchDescription(
         [
             use_sim_time_arg,
             joy_teleop,
-            joy_node
+            joy_node,
+            twist_mux_launch,
+            twist_relay_node,
         ]
     )
