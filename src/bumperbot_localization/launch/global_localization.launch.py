375c46195dcfbf4757650809a573572f2bcd5d74
shourya <pihushourya100@gmail.com>
2026-05-06 21:43:02 +0530

    checked examples of cpp and py, added lifecycle manager in global_localization.launch.py and found bug in the same file, but fixed it by adding manager

--- src/bumperbot_localization/launch/global_localization.launch.py
+++ src/bumperbot_localization/launch/global_localization.launch.py
@@ -1,24 +1,25 @@
 from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
 
-from ament_index_python import get_package_share_directory
 
 def generate_launch_description():
 
-    use_sim_time_arg = DeclareLaunchArgument(
-        "use_sim_time",
-        default_value="False",
-    )
+    map_name = LaunchConfiguration("map_name")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    lifecycle_nodes = ["map_server"]
 
     map_name_arg = DeclareLaunchArgument(
         "map_name",
-        default_value="small_house",
+        default_value="small_house"
     )
 
-    map_name = LaunchConfiguration("map_name")
-    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="true"
+    )
 
     map_path = PathJoinSubstitution([
         get_package_share_directory("bumperbot_mapping"),
@@ -26,18 +27,33 @@ def generate_launch_description():
         map_name,
         "map.yaml"
     ])
-
+    
     nav2_map_server = Node(
         package="nav2_map_server",
         executable="map_server",
+        name="map_server",
         output="screen",
         parameters=[
-            {"yaml_filename": map_path,
-             "use_sim_time": use_sim_time}
-        ]
+            {"yaml_filename": map_path},
+            {"use_sim_time": use_sim_time}
+        ],
     )
+
+    nav2_lifecycle_manager = Node(
+        package="nav2_lifecycle_manager",
+        executable="lifecycle_manager",
+        name="lifecycle_manager_localization",
+        output="screen",
+        parameters=[
+            {"node_names": lifecycle_nodes},
+            {"use_sim_time": use_sim_time},
+            {"autostart": True}
+        ],
+    )
+
     return LaunchDescription([
-        use_sim_time_arg,
         map_name_arg,
-        nav2_map_server
+        use_sim_time_arg,
+        nav2_map_server,  
+        nav2_lifecycle_manager,
     ])
